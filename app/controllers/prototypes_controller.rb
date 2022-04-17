6a2457a8d1ceda004b20f52b1f679cc03f71cd9f
yuta001026 <97606911+yuta001026@users.noreply.github.com>
2022-04-17 18:11:15 +0900

    commit11

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index, :show]
 
   def new
     @prototype = Prototype.new
