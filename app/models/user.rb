88769eb4e0a303d900eecf00505625fb9aa0123c
yuta001026 <97606911+yuta001026@users.noreply.github.com>
2022-04-17 11:59:06 +0900

    commit8

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          validates :name, presence: true, length: { maximum: 6 }
+         validates :profile, presence: true
+         validates :occupation, presence: true
+         validates :position, presence: true
          has_many :prototypes
          has_many :comments, dependent: :destroy
 end
